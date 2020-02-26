c7d2975b200eef91b8e6ab77e66dc50d6c80d482
DiDia <mikegoescoding@gmail.com>
2020-02-26 00:25:38 -0700

    first commit backup laptop

---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>Backup Laptop Test</h1>
+        <h2>Running Smoothly...&#128077;</h2>
+      </div>
+    );
+  }
+}
